28f4878b0925b7c74396ee33733c78b2fab29aad
Paul Johnson <pjohnson.cv@gmail.com>
2021-09-29 17:50:00 +0100

    Started work on processing CSV file into database.
    test.py renamed db_methods.py, tidied up to be less hackish
    All queries are encapsulated in try/except. Prints error, or continues
    silently (return values, no print).
    - initialiseTables
      Intended to be run when starting the program to establish required
      table structure
    - initialiseAnalytes
      Intended to be run when starting the program to insert known analyte
      parameter data.
    - selectPatientCount
      Returns the number of rows which have a matching patient ID, use to
      figure out if we need to insert the patient
    - insertNewPatient
      PLACEHOLDER method.
    
    csv_parser.py - NEW
    Added methods to select a file or fail gracefully, and the beginnings
    of parsing each row by use of the csv DictReader.

---
+++ db_methods.py
@@ -0,0 +1,112 @@
+import sqlite3
+from sqlite3 import Error
+
+dbConn = sqlite3.connect("dbtest.db") #(":memory:") #"/mnt/chromeos/MyFiles/Downloads/rawdata.db")
+dbCurs = dbConn.cursor()
+
+define_tables = """
+  CREATE TABLE IF NOT EXISTS patient (
+    study_id INTEGER PRIMARY KEY,
+    date_of_birth TEXT NOT NULL,
+    sex INTEGER DEFAULT 1,
+    ethnicity INTEGER DEFAULT 0
+  );
+  
+  CREATE TABLE IF NOT EXISTS sample (
+    samp_key INTEGER PRIMARY KEY AUTOINCREMENT,
+    samp_id_full TEXT UNIQUE,
+    receipt_date TEXT NOT NULL,
+    type INTEGER DEFAULT 1
+  );
+  
+  CREATE TABLE IF NOT EXISTS patient_sample (
+    study_id INTEGER,
+    samp_key INTEGER,
+    PRIMARY KEY (study_id, samp_key),
+    FOREIGN KEY (study_id) REFERENCES patient (study_id) ON DELETE CASCADE ON UPDATE NO ACTION,
+    FOREIGN KEY (samp_key) REFERENCES sample (samp_key) ON DELETE CASCADE ON UPDATE NO ACTION
+  );
+  
+  CREATE TABLE IF NOT EXISTS analyte (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    code TEXT NOT NULL UNIQUE,
+    descriptor TEXT NOT NULL,
+    units TEXT NOT NULL
+  );
+  
+  CREATE TABLE IF NOT EXISTS result (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    analyte_id INTEGER,
+    value TEXT,
+    comment TEXT,
+    FOREIGN KEY (analyte_id) REFERENCES analyte (id)
+  );
+  
+  CREATE TABLE IF NOT EXISTS sample_result (
+    samp_key INTEGER,
+    result_id INTEGER,
+    PRIMARY KEY (samp_key, result_id),
+    FOREIGN KEY (samp_key) REFERENCES sample (samp_key) ON DELETE CASCADE ON UPDATE NO ACTION,
+    FOREIGN KEY (samp_key) REFERENCES result (id) ON DELETE CASCADE ON UPDATE NO ACTION
+  );
+"""
+known_analytes = [
+  ("SOD", "Sodium", "mmol/L"),
+  ("POT", "Potassium", "mmol/L"),
+  ("URE", "Urea", "mmol/L"),
+  ("CRE", "Creatinine", "umol/L"),
+  ("GFR", "eGFR (old/U&E)", "mL/min/1.73m^2"),
+  ("GFRE", "eGFR", "mL/min/1.73m^2"),
+  ("AKI", "AKI", "(score)"),
+  ("UMICR", "Albumin Ratio", "mg/mmol"),
+  ("CRP", "C.Reactive Protein", "mg/L"),
+  ("IHBA1C", "HbA1c", "mmol/mo"),
+  ("HB", "Haemoglobin", "g/L"),
+  ("HCT", "Haematocrit", "L/L"),
+  ("MCH", "Mean cell haemoglobin", "pg"),
+  ("PHO", "Phosphate", "mmol/L"),
+  ("DEMO","DEMO - DELETE", "DEMO")
+]
+
+def initialiseTables():
+  try: 
+    dbCurs.executescript(define_tables)
+    dbConn.commit()
+  except Error as e:
+	  print("ERROR [db_methods.initialiseTables]: ", e)
+
+def initialiseAnalytes():
+  define_analytes = """
+    INSERT OR IGNORE INTO analyte (code, descriptor, units) VALUES (?, ?, ?);
+  """
+
+  try:
+    dbCurs.executemany(define_analytes, known_analytes)
+    dbConn.commit() 
+    print("Inserted new analyte(s): ", dbCurs.rowcount)
+  except Error as e:
+    print("ERROR [db_methods.initialiseAnalytes]: ", e)
+
+def selectPatientCount(id=None):
+  try:
+    dbCurs.execute("""SELECT COUNT(study_id) FROM patient WHERE study_id=?;""", [id])
+    rows = dbCurs.fetchall()
+    for row in rows:
+      return row[0]
+  except Error as e:
+    print("ERROR [db_methods.selectPatientCount]: ", e)
+
+def insertNewPatient(study_id, date_of_birth, sex, ethnicity):
+  return False
+
+#initialiseTables()
+#initialiseAnalytes()
+
+#try:
+#  dbCurs.execute("""SELECT * FROM analytes;""")
+#  rows = dbCurs.fetchall()
+#  for row in rows:
+#	  print(row)
+#except Error as e:
+#    print("Ya goofed ", e)
+#dbConn.close()
